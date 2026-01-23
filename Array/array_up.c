51ecc4932edc87a9cbe43ba11537e065ecceb52a
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-01-23 16:16:15 +0600

    updation of array

---
+++ Array/array_up.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = 10, k = 4, new = 23;
+    int i;
+    printf("Array:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Array[%d]=%d\n", i, arr[i]);
+    }
+    arr[k-1] = new;
+    printf("After Updation:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Array[%d]=%d\n", i, arr[i]);
+    }
+    return 0;
+}
