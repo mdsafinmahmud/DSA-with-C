63ab6f2ee2a0d453598ae3378117c4de6deb0688
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-01-30 21:18:17 +0600

    Update Practice_code_4.c

--- Array/Practice/Practice_code_4.c
+++ Array/Practice/Practice_code_4.c
@@ -3,21 +3,32 @@
 int main()
 {
     int i, n, target;
-    printf("Enter Array Size: ");
+    printf("Enter array SIZE: ");
     scanf("%d", &n);
     int arr[n];
-    printf("Enter %d Elements :\n ");
+    printf("Enter %d Elements: ", n);
+
     for (i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    printf("Enter number to search:");
+    printf("Enter number to search: ");
     scanf("%d", &target);
     int fnd = 0;
     for (i = 0; i < n; i++)
+
     {
         if (arr[i] == target)
-            printf("Found it at index %d", i);
+        {
+            printf("Found it at index %d\n", i);
+            fnd = 1;
+            break;
+        }
     }
+    if (fnd == 0)
+    {
+        printf("NOT fount!\n");
+    }
+
     return 0;
 }
